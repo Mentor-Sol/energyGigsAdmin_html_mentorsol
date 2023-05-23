6c12dc8fa27fbf5e964ca5195bff180e308b4ed0
FahadAziz-Mentorsol <132256870+FahadAziz-Mentorsol@users.noreply.github.com>
2023-05-23 19:34:59 +0500

    signup +  profile + Login + Creation Project

--- src/assets/js/energygigs/location-preference.js
+++ src/assets/js/energygigs/location-preference.js
@@ -1,8 +1,37 @@
-<script>
-  $(document).ready(function(){" "}
-  {$(".selectable-card").click(function () {
-    $(".selectable-card").removeClass("selected");
-    $(this).addClass("selected");
-  })}
-  );
-</script>;
+function toggleSelection() {
+  var checkbox = document.getElementById("checkbox");
+  var card = document.querySelector(".selectable-card");
+  var imageContainer = card.querySelector(".image-container");
+
+  checkbox.checked = !checkbox.checked;
+  card.classList.toggle("selected");
+  imageContainer.classList.toggle("selected");
+
+  var image = card.querySelector(".image");
+  if (checkbox.checked) {
+    // Checkbox is checked
+    image.style.border = "4px solid #ff0000";
+  } else {
+    // Checkbox is unchecked
+    image.style.border = "";
+  }
+}
+
+function toggleSelection() {
+  var checkbox = document.getElementById("checkbox-2");
+  var card = document.querySelector(".selectable-card-2");
+  var imageContainer = card.querySelector(".image-container-2");
+
+  checkbox.checked = !checkbox.checked;
+  card.classList.toggle("selected");
+  imageContainer.classList.toggle("selected");
+
+  var image = card.querySelector(".image-2");
+  if (checkbox.checked) {
+    // Checkbox is checked
+    image.style.border = "4px solid #ff0000";
+  } else {
+    // Checkbox is unchecked
+    image.style.border = "";
+  }
+}
